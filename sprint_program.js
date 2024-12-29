54576bc4cb859c76a27e8ff78a89435fababcbed
malleswaribatta <183718953+malleswaribatta@users.noreply.github.com>
2024-12-29 17:32:01 +0530

    after cleaning

--- sprint_program.js
+++ sprint_program.js
@@ -1,8 +1,6 @@
-import { test } from "/Users/battamalleswari/workspace/objects/Assignment/js-import-export-malleswaribatta/functions_test.js";
+// const sprintCode = [3, 3, 9, 7, 2, 6, 0, 3, 2, 0, 10];
 
-const sprintCode = [1, 5, 6, 7, 9, 1, 2, 0];
-
-const copy = (index) => {
+const copy = (sprintCode, index) => {
   const fromIndex = sprintCode[index + 1];
   const toIndex = sprintCode[index + 2];
 
@@ -10,31 +8,37 @@ const copy = (index) => {
   return index + 3;
 };
 
-const stop = () => sprintCode.length;
+const stop = (sprintCode) => sprintCode.length;
 
-const jump = (index) => {
+const jump = (sprintCode, index) => {
   return sprintCode[index + 1];
 };
 
-const add = (index) => {
-  const value1Index = sprintCode[index + 1];
-  const value2Index = sprintCode[index + 2];
-  const resultIndex = sprintCode[index + 3];
+const getIndexs = (sprintCode, index) => {
+  const index1 = sprintCode[index + 1];
+  const index2 = sprintCode[index + 2];
+  const index3 = sprintCode[index + 3];
+
+  return [index1, index2, index3];
+};
+
+const add = (sprintCode, index) => {
+  const [value1Index, value2Index, resultIndex] = getIndexs(index);
+
   sprintCode[resultIndex] = sprintCode[value1Index] + sprintCode[value2Index];
 
   return index + 4;
 };
 
-const sub = (index) => {
-  const value1Index = sprintCode[index + 1];
-  const value2Index = sprintCode[index + 2];
-  const resultIndex = sprintCode[index + 3];
+const sub = (sprintCode, index) => {
+  const [value1Index, value2Index, resultIndex] = getIndexs(sprintCode, index);
+
   sprintCode[resultIndex] = sprintCode[value1Index] - sprintCode[value2Index];
 
   return index + 4;
 };
 
-const put = (index) => {
+const put = (sprintCode, index) => {
   const value = index + 1;
   const toIndex = sprintCode[index + 2];
 
@@ -43,62 +47,74 @@ const put = (index) => {
   return index + 3;
 };
 
-const equal = (index) => {
-  const value1Index = sprintCode[index + 1];
-  const value2Index = sprintCode[index + 2];
-  const jumpTo = sprintCode[index + 3];
+const equal = (sprintCode, index) => {
+  const [value1Index, value2Index, jumpTo] = getIndexs(index);
+
   const value1 = sprintCode[value1Index];
   const value2 = sprintCode[value2Index];
 
-  if (value1 === value2) {
-    return jumpTo;
-  }
+  const goTo = value1 === value2 ? jumpTo : index + 4;
 
-  return index + 4;
+  return goTo;
 };
 
-const lessThan = (index) => {
-  const value1Index = sprintCode[index + 1];
-  const value2Index = sprintCode[index + 2];
-  const jumpTo = sprintCode[index + 3];
+const lessThan = (sprintCode, index) => {
+  const [value1Index, value2Index, jumpTo] = getIndexs(index);
+
   const value1 = sprintCode[value1Index];
   const value2 = sprintCode[value2Index];
+  const goTo = value1 < value2 ? jumpTo : index + 4;
 
-  if (value1 < value2) {
-    return jumpTo;
-  }
-
-  return index + 4;
+  return goTo;
 };
 
-const instructions = {
-  0: put,
-  1: add,
-  2: sub,
-  3: jump,
-  4: equal,
-  5: lessThan,
-  7: copy,
-  9: stop,
-};
+const main = (value, index, sprintCode) => {
+  const instructions = {
+    0: put,
+    1: add,
+    2: sub,
+    3: jump,
+    4: equal,
+    5: lessThan,
+    7: copy,
+    9: stop,
+  };
 
-const main = (value, index) => {
   if (value in instructions) {
     const fun = instructions[value];
-    index = fun(index);
+    index = fun(sprintCode, index);
+    return index;
   }
 
-  return index;
+  console.log("unknown instuction at index:", index + 1);
+  return sprintCode.length;
 };
 
-const executeSprintCode = () => {
+const getIndex = (sprintCode) => {
+  let indexs = "";
+
+  for (let i = 1; i <= sprintCode.length; i++) {
+    indexs += "  " + i + " ";
+  }
+
+  return indexs;
+};
+
+const executeSprintCode = (sprintCode) => {
   let index = 0;
 
   while (index < sprintCode.length) {
-    index = main(sprintCode[index], index);
+    index = main(sprintCode[index], index, sprintCode);
   }
 
-  console.log(test([1, 5, 6, 7, 9, 1, 2, 3], sprintCode));
+  const indexs = getIndex(sprintCode);
+  const times = sprintCode.length * 4.1;
+  const line = "\n" + "-".repeat(times) + "\n";
+  const box = sprintCode.map((element) => "| " + element).join(" ") + " |";
+
+  const result = line + box + line + indexs;
+
+  return result;
 };
 
-executeSprintCode();
+console.log(executeSprintCode([3, 3, 9, 7, 2, 6, 0, 3, 2, 0, 10]));
